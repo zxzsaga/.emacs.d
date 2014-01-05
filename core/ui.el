cf74ed94260f2782e698efa4a44bda9501f74cf7
Cielmer <zxzsaga@gmail.com>
2014-01-06 00:37:04 +0800

    first commit

---
+++ core/ui.el
@@ -0,0 +1,45 @@
+;; the toolbar is just a waste of valuable screen estate
+;; in a tty tool-bar-mode does not properly auto-load, and is
+;; already disabled anyway
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(menu-bar-mode -1)
+
+;; the blinking cursor is nothing, but an annoyance
+(blink-cursor-mode -1)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; nice scrolling
+(setq scroll-margin 2
+      scroll-conservatively 1000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; make the fringe (gutter) smaller
+;; the argument is a width in pixels (the default is 8)
+(if (fboundp 'fringe-mode)
+    (fringe-mode 4))
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; more useful frame title, that show either a file or a
+;; buffer name (if the buffer isn't visiting a file)
+(setq frame-title-format
+      '("" invocation-name " Prelude - " (:eval (if (buffer-file-name)
+                                            (abbreviate-file-name (buffer-file-name))
+                                          "%b"))))
+
+;; use blackboard as the default theme
+(load-theme 'blackboard t)
+
+(provide 'ui)
+;;; prelude-ui.el ends here
+
---
+++ init.el
@@ -0,0 +1,62 @@
+(defvar current-user
+  (getenv
+   (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+(when (version< emacs-version "24.1")
+  (error "Requires at least GNU Emacs 24.1"))
+
+(defvar root-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs configuration.")
+(defvar core-dir (expand-file-name "core" root-dir)
+  "The home of core functionality.")
+(defvar modules-dir (expand-file-name "modules" root-dir)
+  "This directory houses all of the built-in modules.")
+(defvar themes-dir (expand-file-name "themes" root-dir)
+  "This directory houses themes.")
+(setq custom-theme-directory themes-dir)
+(defvar vendor-dir (expand-file-name "vendor" root-dir)
+  "This directory houses packages that are not yet available in ELPA (or MELPA).")
+(defvar savefile-dir (expand-file-name "savefile" root-dir)
+  "This folder stores all the automatically generated save/history-files.")
+(defvar load-modules-file (expand-file-name "load-modules.el" root-dir)
+  "This files contains a list of modules that will be loaded.")
+
+(unless (file-exists-p savefile-dir)
+  (make-directory savefile-dir))
+
+(defun add-subfolders-to-load-path (parent-dir)
+  "Add all level PARENT-DIR subdirs to the `load-path'."
+  (dolist (f (directory-files parent-dir))
+    (let ((name (expand-file-name f parent-dir)))
+      (when (and (file-directory-p name)
+		 (not (equal f ".."))
+		 (not (equal f ".")))
+	(add-to-list 'load-path name)
+	(add-subfolders-to-load-path name)))))
+
+;; add Prelude's directories to Emacs's `load-path'
+(add-to-list 'load-path core-dir)
+(add-to-list 'load-path modules-dir)
+(add-to-list 'load-path vendor-dir)
+(add-subfolders-to-load-path vendor-dir)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; the core stuff
+(require 'packages)
+(require 'ui)
+(require 'core)
+(require 'mode)
+(require 'editor)
+(require 'global-keybindings)
+
+;; OSX specific settings
+(when (eq system-type 'darwin)
+  (require 'osx))
+
+;; the modules
+(when (file-exists-p load-modules-file)
+  (load load-modules-file))
+
+;;; init.el ends here
